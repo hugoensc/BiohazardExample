fac02d4cb9e307ca72e3b3574d16d6eba4448cd4
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2020-02-11 15:18:43 -0600

    FirstCommit

---
+++ BiohazardExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BiohazardExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
